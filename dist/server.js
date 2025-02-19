a3887290b34ca333a238265b7159e1ceab90e73e
Michelle <michelleh11219@gmail.com>
2025-02-19 00:36:47 -0500

    remove extraneous file

--- dist/server.js
+++ dist/server.js
@@ -18,6 +18,6 @@ app.get("/", (req, res) => {
     res.json({ message: "Hello, World!" });
 });
 const port = process.env.PORT || 8000;
-const server = app.listen(port, () => {
+app.listen(port, () => {
     console.log(`server running on port ${port}`);
 });
