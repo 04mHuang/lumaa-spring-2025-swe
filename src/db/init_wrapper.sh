6736ead455ee5f29aae7acd6b2e3ffeb9027bfe4
Michelle <michelleh11219@gmail.com>
2025-02-21 23:53:12 -0500

    add port and host

--- src/db/init_wrapper.sh
+++ src/db/init_wrapper.sh
@@ -9,4 +9,6 @@ sudo -u postgres psql \
   -v db_user="$DB_USER" \
   -v db_password="$DB_PASSWORD" \
   -v db_name="$DB_NAME" \
+  -v db_port="$DB_PORT" \
+  -v db_host="$DB_HOST" \
   -f src/db/init.sql
